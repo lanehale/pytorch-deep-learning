ef1bf5ecec481033641312351cdb258352beea0c
lanehale <lanehale1@gmail.com>
2025-05-23 16:40:21 -0600

    Update engine.py
    add tensorboard install

--- going_modular/engine.py
+++ going_modular/engine.py
@@ -6,6 +6,17 @@ import torch
 from tqdm.auto import tqdm
 from typing import Dict, List, Tuple
 
+# See if tensorboard exists, if not, install it
+try:
+  from torch.utils.tensorboard import SummaryWriter
+  print("SummaryWriter already installed.")
+except:
+  print("Installing SummaryWriter...")
+  !pip install -q tensorboard
+  from torch.utils.tensorboard import SummaryWriter
+  print("Done installing SummaryWriter.")
+
+
 def train_step(model: torch.nn.Module,
                dataloader: torch.utils.data.DataLoader,
                loss_fn: torch.nn.Module,
