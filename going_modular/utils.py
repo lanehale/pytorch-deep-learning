78057e5824db6a6ff9e9b527e0e2f58814948f2f
lanehale <lanehale1@gmail.com>
2025-06-09 18:01:33 -0600

    Update utils.py
    add set_seeds()

--- going_modular/utils.py
+++ going_modular/utils.py
@@ -212,3 +212,17 @@ def compare_results(pred_list, name, col1_width):
       f"or {false_percent:5.2f}% wrong, "
       f"{(100.0 - false_percent):.2f}% right"
   )
+
+
+"""
+Create a function to set seeds
+"""
+def set_seeds(seed: int=42):
+  """Sets random sets for torch operations.
+
+  Args:
+    seed (int, optional): Random seed to set. Defaults to 42.
+  """
+  # Set the seed for general torch operations
+  torch.manual_seed(seed)
+  torch.cuda.manual_seed(seed)
